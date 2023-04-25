d5183d3550116239d2393b96ec3dcc90e99fbf0e
kyeon06 <jkyeon06@gmail.com>
2023-04-25 14:52:28 +0900

    :tada: [init] todo project 초기 세팅

---
+++ src/main/java/com/yuyun/todochecker/TodoCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.yuyun.todochecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuyun/todochecker/TodoCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuyun.todochecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
