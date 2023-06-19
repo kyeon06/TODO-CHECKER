f1a73647f32c157b965c8de2974ac5b561414133
kyeon06 <111492415+kyeon06@users.noreply.github.com>
2023-06-20 01:34:02 +0900

    라벨 filter 조회 추가 (#19)
    * :sparkles: [feat] 라벨 필터링 조회 controller
    
    * :sparkles: [fix] 명언 내용 수정
    
    * :sparkles: create response list dto
    
    * :sparkles: [fix] filter controller
    
    * :sparkles: [feat] create filter service

--- src/main/java/com/yuyun/todochecker/todo/controller/TodoController.java
+++ src/main/java/com/yuyun/todochecker/todo/controller/TodoController.java
@@ -3,12 +3,16 @@ package com.yuyun.todochecker.todo.controller;
 import com.yuyun.todochecker.todo.dto.PostRequestDto;
 import com.yuyun.todochecker.todo.dto.ProgressDto;
 import com.yuyun.todochecker.todo.dto.TodoDto;
+import com.yuyun.todochecker.todo.dto.TodoListDto;
 import com.yuyun.todochecker.todo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/todo")
@@ -44,6 +48,13 @@ public class TodoController {
     @RequestMapping(value = "/{todoId}", method = RequestMethod.PUT)
     public ResponseEntity<TodoDto> updateToDo(@PathVariable("todoId") final long todoId, @RequestBody final TodoDto todoDto) {
         TodoDto res = todoService.updateToDo(todoId, todoDto);
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    // 일별 목록 필터 목록 조회
+    @RequestMapping(value = "/{runDate}/{labelId}", method = RequestMethod.GET)
+    public ResponseEntity<TodoListDto> getFilterList(@PathVariable("runDate") String runDate, @PathVariable(value = "labelId") final long label) {
+        TodoListDto res = todoService.getFilter(runDate, label);
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 }
